54a9386a46d232e83fe9b54c6ee62cb033abb946
Totskiy Pavel <tockiypav@icloud.com>
2024-09-20 17:44:22 +0500

    Initial Commit

---
+++ SoundScape/SoundScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundScapeApp.swift
+//  SoundScape
+//
+//  Created by Pavel Totskiy on 20.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SoundScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoundScapeTests/SoundScapeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoundScapeTests.swift
+//  SoundScapeTests
+//
+//  Created by Pavel Totskiy on 20.09.2024.
+//
+
+import Testing
+@testable import SoundScape
+
+struct SoundScapeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
